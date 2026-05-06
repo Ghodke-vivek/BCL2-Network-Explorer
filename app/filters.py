1bb465dd384e5734901f79390e62c8e1b8e8f5c9
Ghodke-vivek <vivekghodke06@gmail.com>
2026-05-06 18:31:46 +0530

    Update filters.py

--- app/filters.py
+++ app/filters.py
@@ -1,13 +1,1 @@
-# Placeholder for future filtering system
 
-
-def filter_nodes_by_type(
-    metadata,
-    node_type
-):
-
-    return {
-        k: v
-        for k, v in metadata.items()
-        if v["type"] == node_type
-    }
